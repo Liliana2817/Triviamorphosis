1a043a63284e4b1d829be7a4e2a3970accae3a77
Kibria <kibriare@gmail.com>
2023-05-25 06:16:12 +0200

    subiendo el ultimo cambio del carril

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -3,10 +3,9 @@ import './css/lista.css';
 
 function Lista() {
   const [lista, setLista] = useState([]);
-  const [carritoVisible, setCarritoVisible] = useState(true);
 
   const anidar = () => {
-    if (localStorage.getItem('carro') !== undefined) {
+    if (localStorage.getItem('carro') !== null) {
       setLista(JSON.parse(localStorage.getItem('carro')));
     } else {
       //setCarrito(JSON.parse(carritoCompra))
@@ -28,50 +27,53 @@ function Lista() {
   const calcularPrecioTotal = () => {
     let total = 0;
     lista.forEach((producto) => {
-      if(producto.precio!='gratis'){
+      if (producto.precio !== 'gratis') {
         total += parseFloat(producto.precio);
       }
     });
-    return total.toFixed(2);
+  
+    // Separar los euros y los centavos con coma en lugar de punto
+    // const [euros, centavos] = total.toFixed(2).split('.');
+    // const precioFormateado = euros + ',' + centavos;
+    const resultado = new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'EUR' }).format(total) 
+  
+    return resultado;
   };
 
   return (
-    <div>
+    <div className="lista-container">
       <br />
       <br />
       <br />
       <br />
-      <p onClick={() => anidar()}>click</p>
 
-      {carritoVisible && (
-      <>
-        <div>
-          {lista.map((index, i) => (
-            
-            <div className="column1" key={i}>
-              <div className="card1">
-                <h3>{index.title}</h3>
-                <div className="precio">
-                  {index.precio} {index.moneda}
-                </div>
-                <div className="p-1">
-                  <ul>
-                    <h2>{index.op3}</h2>
-                    <li>{index.op1}</li>
-                    <li>{index.op2}</li>
-                  </ul>
-                </div>
+      {lista.length === 0 ? (
+          <h2 className="empty-message">No tienes ningún producto :(</h2>
+      ) : (
+        <>
+          <div>
+            {lista.map((index, i) => (
+              <div className="column1" key={i}>
+                <div className="card1">
+                  <h3>{index.title}</h3>
+                  <div className="precio">
+                    {index.precio} {index.moneda}
+                  </div>
+                  <div className="p-1">
+                    <ul>
+                      <h2>{index.op3}</h2>
+                      <li>{index.op1}</li>
+                      <li>{index.op2}</li>
+                    </ul>
+                  </div>
 
-                <div className="usuario">{index.invited}</div>
-                <button className="botton" onClick={() => quitarCarrito(i)}>Quitar carrito</button>
+                  <div className="usuario">{index.invited}</div>
+                  <button className="botton" onClick={() => quitarCarrito(i)}>Quitar producto</button>
+                </div>
               </div>
-            </div>
-            
-          ))}
-          
-          
-        </div>
-        <h2 className='total-carrito' >Total precio: {calcularPrecioTotal()} €</h2>
+            ))}
+          </div>
+          <h2 className='total-carrito'>Total precio: {calcularPrecioTotal()} </h2>
         </>
       )}
     </div>
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,48 +1,39 @@
 import { Link } from 'react-router-dom';
-import {useState} from 'react';
-import valores from "../data/datos_valor.json";
-import './css/productos.css'
-
+import { useState } from 'react';
+import valores from '../data/datos_valor.json';
+import './css/productos.css';
 
 function Productos() {
-    // const carritoCompra = {'item':2,'item':1, 'item':3}
-    const [carrito, setCarrito] = useState([])
-    const anidar = (datos) =>{
-      console.log('anidar'+datos)
-      if(localStorage.getItem('carro')!==undefined){
-        setCarrito(JSON.parse(localStorage.getItem('carro')));
-      }else{
-        //setCarrito(JSON.parse(carritoCompra))
-      }
-                                                                                    
-      console.log('carrito '+ carrito)
-      const carro = [...carrito, datos]
-      setCarrito(carro)
-      localStorage.setItem('carro',JSON.stringify(carrito))
-    }
-    return (
-      <main>
-        <h2>Elija un plan que se adapte a sus necesidades de participación</h2>
-        <div class="row">
-        { 
-          valores.map((datos, id)=>(
-            <div class="column">
-              <div class="card">
-                <h3>{datos.title}</h3>
-                <div className='precio'>{datos.precio} {datos.moneda}</div>
-                <p>{datos.op1}</p>
-                <p>{datos.op2}</p>
-                <p>{datos.op3}</p>
-                <div className='usuario'>{datos.invited}</div>
-                <button onClick={()=>anidar(datos)}>Seleccionar plan</button>
-              </div>
-            </div>
-          ))
-        }
-        </div>
-      </main>
+  const [carrito, setCarrito] = useState([]);
 
-    );
+  const anidar = (datos) => {
+    const carro = [...carrito, datos];
+    setCarrito(carro);
+    localStorage.setItem('carro', JSON.stringify(carro));
   };
-  
+
+  return (
+    <main>
+      <h2>Elija un plan que se adapte a sus necesidades de participación</h2>
+      <div className="row">
+        {valores.map((datos, id) => (
+          <div className="column" key={id}>
+            <div className="card">
+              <h3>{datos.title}</h3>
+              <div className="precio">
+                {datos.precio} {datos.moneda}
+              </div>
+              <p>{datos.op1}</p>
+              <p>{datos.op2}</p>
+              <p>{datos.op3}</p>
+              <div className="usuario">{datos.invited}</div>
+              <button onClick={() => anidar(datos)}>Seleccionar plan</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
+
 export default Productos;
